bff61cd9ee0313da17f602ec097c66d69ab11567
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-05-17 09:13:42 +0800

    Complete orders

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { OrdersService } from "../shared/orders.service";
 
 @Component({
-  selector: 'app-orders',
-  templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+  selector: "app-orders",
+  templateUrl: "./orders.component.html",
+  styleUrls: ["./orders.component.scss"]
 })
 export class OrdersComponent implements OnInit {
+  coffees = [
+    "Americano",
+    "Flat White",
+    "Cappuccino",
+    "Latte",
+    "Espresso",
+    "Machiato",
+    "Mocha",
+    "Hot Chocolate",
+    "Tea"
+  ];
 
-  constructor() { }
+  coffeeOrder = [];
 
-  ngOnInit() {
-  }
+  constructor(private ordersService: OrdersService) {}
 
+  ngOnInit() {}
+
+  addCoffee = coffee => this.coffeeOrder.push(coffee);
+
+  removeCoffee = coffee => {
+    const index = this.coffeeOrder.indexOf(coffee);
+    if (index > -1) this.coffeeOrder.splice(index, 1);
+  };
+
+  onSubmit() {}
 }
